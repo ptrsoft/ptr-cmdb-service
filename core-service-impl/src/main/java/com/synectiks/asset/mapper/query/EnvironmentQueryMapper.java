a2073bd24a86631fa40e12c8ddc53907b2e8c1b9
ManojSynectiks <manoj.sharma@synectiks.com>
2023-08-28 13:57:41 +0530

    landingzone database id included in /environment/summary-list api

--- core-service-impl/src/main/java/com/synectiks/asset/mapper/query/EnvironmentQueryMapper.java
+++ core-service-impl/src/main/java/com/synectiks/asset/mapper/query/EnvironmentQueryMapper.java
@@ -1,14 +1,17 @@
 package com.synectiks.asset.mapper.query;
 
 import com.synectiks.asset.api.model.EnvironmentQueryDTO;
+import com.synectiks.asset.api.model.EnvironmentSummaryQueryDTO;
 import com.synectiks.asset.domain.query.EnvironmentQueryObj;
 import com.synectiks.asset.domain.query.EnvironmentSummaryQueryObj;
 
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Mapper
@@ -19,9 +22,24 @@ public interface EnvironmentQueryMapper {
     EnvironmentQueryDTO toDto(EnvironmentQueryObj environmentQueryObj);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    List<EnvironmentQueryDTO> toDtoList(List<EnvironmentQueryObj> environmentQueryObjList);
+    @Mapping(target = "landingZoneId", source = "id")
+    default List<EnvironmentQueryDTO> toDtoList(List<EnvironmentQueryObj> environmentQueryObjList){
+        List<EnvironmentQueryDTO> environmentQueryDTOList = new ArrayList<>();
+        for(EnvironmentQueryObj environmentQueryObj: environmentQueryObjList){
+            EnvironmentQueryDTO environmentQueryDTO = new EnvironmentQueryDTO();
+            environmentQueryDTO.setCloud(environmentQueryObj.getCloud());
+            List<EnvironmentSummaryQueryDTO> environmentSummaryQueryDTOList = new ArrayList<>();
+            for(EnvironmentSummaryQueryObj environmentSummaryQueryObj: environmentQueryObj.getEnvironmentSummaryList()){
+                EnvironmentSummaryQueryDTO environmentSummaryQueryDTO = EnvironmentSummaryQueryMapper.INSTANCE.toDto(environmentSummaryQueryObj);
+                environmentSummaryQueryDTOList.add(environmentSummaryQueryDTO);
+            }
+            environmentQueryDTO.setEnvironmentSummaryList(environmentSummaryQueryDTOList);
+            environmentQueryDTOList.add(environmentQueryDTO);
+        }
+        return environmentQueryDTOList;
+    }
     
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    List<EnvironmentQueryDTO> towDtoList(List<EnvironmentSummaryQueryObj> environmentQueryObjList);
+//    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+//    List<EnvironmentQueryDTO> towDtoList(List<EnvironmentSummaryQueryObj> environmentQueryObjList);
 
 }
--- core-service-impl/src/main/java/com/synectiks/asset/mapper/query/EnvironmentSummaryQueryMapper.java
+++ core-service-impl/src/main/java/com/synectiks/asset/mapper/query/EnvironmentSummaryQueryMapper.java
@@ -4,6 +4,7 @@ import com.synectiks.asset.api.model.EnvironmentSummaryQueryDTO;
 import com.synectiks.asset.domain.query.EnvironmentSummaryQueryObj;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
@@ -14,6 +15,7 @@ public interface EnvironmentSummaryQueryMapper {
     EnvironmentSummaryQueryMapper INSTANCE = Mappers.getMapper(EnvironmentSummaryQueryMapper.class);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "landingZoneId", source = "id")
     EnvironmentSummaryQueryDTO toDto(EnvironmentSummaryQueryObj environmentSummaryQueryObj);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
