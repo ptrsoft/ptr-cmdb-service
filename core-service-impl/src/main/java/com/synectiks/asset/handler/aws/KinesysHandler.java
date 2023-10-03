2b4930ef6148d86f4fcdddb7cbe2abf0dc695e6a
ManojSynectiks <manoj.sharma@synectiks.com>
2023-10-03 22:41:56 +0530

    tag handling included in kinesys

--- core-service-impl/src/main/java/com/synectiks/asset/handler/aws/KinesysHandler.java
+++ core-service-impl/src/main/java/com/synectiks/asset/handler/aws/KinesysHandler.java
@@ -17,9 +17,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class KinesysHandler implements CloudHandler {
@@ -47,12 +45,14 @@ public class KinesysHandler implements CloudHandler {
     @Autowired
     private VaultService vaultService;
 
+    @Autowired
+    private TagProcessor tagProcessor;
+
     @Override
     public void save(Organization organization, Department department, Landingzone landingZone, String awsRegion) {
         Object response = getResponse(vaultService, restTemplate, getUrl(), organization, department, landingZone, awsRegion);
-        if(response != null && response.getClass().getName().equalsIgnoreCase("java.util.LinkedHashMap")){
-            Map responseMap = (LinkedHashMap)response;
-            List responseList = (List)responseMap.get("StreamSummaries");
+        if(response != null && response.getClass().getName().equalsIgnoreCase("java.util.ArrayList")){
+            List responseList = (ArrayList)response;
             for(Object obj: responseList){
                 Map configMap = (Map)obj;
                 addUpdate(department, landingZone, configMap);
@@ -61,7 +61,7 @@ public class KinesysHandler implements CloudHandler {
     }
 
     private void addUpdate(Department department, Landingzone landingZone, Map configMap) {
-        String instanceId = (String)configMap.get("StreamName");
+        String instanceId = (String)((Map)((Map)configMap.get("stream")).get("StreamDescription")).get("StreamName");
         CloudElement cloudElement =  cloudElementService.getCloudElementByInstanceId(landingZone.getId(), instanceId, Constants.KINESYS);
         if(cloudElement != null ){
             logger.debug("Updating kinesys: {} for landing-zone: {}",instanceId, landingZone.getLandingZone());
@@ -73,7 +73,7 @@ public class KinesysHandler implements CloudHandler {
             logger.debug("Adding kinesys: {} for landing-zone: {}",instanceId, landingZone.getLandingZone());
             CloudElement cloudElementObj = CloudElement.builder()
                     .elementType(Constants.KINESYS)
-                    .arn((String)configMap.get("StreamARN"))
+                    .arn((String)((Map)((Map)configMap.get("stream")).get("StreamDescription")).get("StreamARN"))
                     .instanceId(instanceId)
                     .instanceName(instanceId)
                     .category(Constants.APP_SERVICES)
@@ -89,4 +89,39 @@ public class KinesysHandler implements CloudHandler {
         return env.getProperty("awsx-api.base-url")+env.getProperty("awsx-api.kinesys-api");
     }
 
+    @Override
+    public Map<String, List<Object>> processTag(CloudElement cloudElement){
+        List<Object> successTagging = new ArrayList<>();
+        List<Object> failureTagging = new ArrayList<>();
+
+        if(cloudElement.getConfigJson() != null ){
+            List tagList = (List)((Map)(((Map)cloudElement.getConfigJson()).get("tags"))).get("Tags");
+            if(tagList != null){
+                for(Object object: tagList){
+                    Map tag = (Map)object;
+                    if(tag.get("Key") != null &&  ((String)tag.get("Key")).toLowerCase().contains("appkube_tag")){
+                        String tagValue[] = ((String)tag.get("Value")).split("/");
+                        Map<String, Object> erroMap = validate(tagValue, cloudElement, tagProcessor);
+                        if(erroMap.size() > 0){
+                            logger.error("Validation error: ",erroMap.get("errorMsg"));
+                            tag.put("failure_reason",erroMap.get("errorMsg"));
+                            tag.put("error_code",erroMap.get("errorCode"));
+                            failureTagging.add(tag);
+                            continue;
+                        }
+                        int errorCode = tagProcessor.process(tagValue, cloudElement);
+                        if(errorCode == 0){
+                            successTagging.add(tag);
+                        }else{
+                            failureTagging.add(tag);
+                        }
+                    }
+                }
+            }
+        }
+        Map<String, List<Object>> statusMap = new HashMap<>();
+        statusMap.put("success", successTagging);
+        statusMap.put("failure",failureTagging);
+        return statusMap;
+    }
 }
